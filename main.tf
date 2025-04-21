4aa5d1dbfb8248860f9f3772b66136b2551bac9d
Ubuntu <azureuser@test-lsh.25htxjpzogkerbdq02ralkokxh.syx.internal.cloudapp.net>
2025-04-21 07:39:19 +0000

    main.tf, terraform.yml 업데이트

--- main.tf
+++ main.tf
@@ -9,15 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc               = true
-  client_id              = "49a5876d-cde1-4724-bad6-d267a5625804"
-  tenant_id              = "03c374dc-f90f-458e-ba2b-d89b9422b208"
-  subscription_id        = "d4adf5c4-07b2-48bf-9105-c296a8353411"
+  use_oidc       = true
+  client_id      = "49a5876d-cde1-4724-bad6-d267a5625804"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "rg-oidc-test"
-  location = "eastus"  # 원하는 지역으로 변경하세요
+  location = "eastus"
   
   tags = {
     environment = "test"
