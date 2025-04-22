62c54524aa7a3d0cae003d3af684edad6917cdd3
Ubuntu <azureuser@test-lsh.25htxjpzogkerbdq02ralkokxh.syx.internal.cloudapp.net>
2025-04-22 05:05:21 +0000

    main.tf update

--- main.tf
+++ main.tf
@@ -11,15 +11,17 @@ provider "azurerm" {
   features {}
 }
 
-# 모든 리소스 그룹 목록 가져오기 - ID 기반으로 확인
-data "azurerm_resource_groups" "all" {}
+# Azure CLI를 사용하여 모든 리소스 그룹 이름 가져오기
+data "external" "resource_groups" {
+  program = ["bash", "-c", "az group list --query \"[].name\" -o json | jq -r '{resource_groups: .}'"]
+}
 
 locals {
   # 기본 리소스 그룹 이름
   base_rg_name = "rg-oidc-test"
   
   # 모든 기존 리소스 그룹 이름 목록
-  existing_rg_names = data.azurerm_resource_groups.all.names
+  existing_rg_names = jsondecode(data.external.resource_groups.result.resource_groups)
   
   # 기본 이름으로 시작하는 리소스 그룹 필터링
   matching_rgs = [for name in local.existing_rg_names : name if startswith(name, local.base_rg_name)]
