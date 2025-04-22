0d1b4c493a819a4dded199c715429a6df2949772
Ubuntu <azureuser@test-lsh.25htxjpzogkerbdq02ralkokxh.syx.internal.cloudapp.net>
2025-04-22 05:32:31 +0000

    update all

--- main.tf
+++ main.tf
@@ -13,18 +13,18 @@ provider "azurerm" {
 
 # Azure CLI를 사용하여 모든 리소스 그룹 이름 가져오기
 data "external" "resource_groups" {
-  program = ["bash", "-c", "az group list --query \"[].name\" | tr -d '\\n' | jq -c '{resource_groups: @json}'"]
+  program = ["bash", "-c", "az group list --query \"[].name\" -o json | jq -r '{resource_groups: .}'"]
 }
 
 locals {
   # 기본 리소스 그룹 이름
-  base_rg_name = "rg-oidc-test"
+  base_rg_name = var.base_name
   
   # 모든 기존 리소스 그룹 이름 목록
   existing_rg_names = jsondecode(data.external.resource_groups.result.resource_groups)
   
-  # 기본 이름으로 시작하는 리소스 그룹 필터링
-  matching_rgs = [for name in local.existing_rg_names : name if startswith(name, local.base_rg_name)]
+  # 기본 이름으로 시작하는 리소스 그룹 필터링 (startswith 대신 substr 사용)
+  matching_rgs = [for name in local.existing_rg_names : name if substr(name, 0, length(local.base_rg_name)) == local.base_rg_name]
   
   # 숫자 버전이 있는 경우 (예: rg-oidc-test-1) 가장 큰 숫자 찾기
   numbered_rgs = [for name in local.matching_rgs : 
@@ -41,7 +41,7 @@ locals {
 
 resource "azurerm_resource_group" "example" {
   name     = local.rg_name
-  location = "eastus"
+  location = var.location
   
   tags = {
     environment = "test"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure 리전"
+  type        = string
+  default     = "eastus"
+}
+
+variable "base_name" {
+  description = "리소스 이름의 기본 부분"
+  type        = string
+  default     = "rg-oidc-test"
+}
