223a3ef3a6ed6582364d4e173a7f61d18bf540f9
Ubuntu <azureuser@test-lsh.25htxjpzogkerbdq02ralkokxh.syx.internal.cloudapp.net>
2025-04-29 07:41:14 +0000

    메인, 변수.tf 커밋

--- main.tf
+++ main.tf
@@ -2,30 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-# 변수 정의
-variable "resource_group_name" {
-  description = "배포할 리소스 그룹 이름"
-  type        = string
-}
-
-variable "user_list" {
-  description = "접근 권한을 부여할 사용자 목록"
-  type        = list(string)
-  default     = []
-}
-
-variable "gpu_size" {
-  description = "필요한 GPU 크기"
-  type        = string
-  default     = "해당 없음"
-}
-
-variable "environment" {
-  description = "배포 환경"
-  type        = string
-  default     = "개발(Development)"
-}
-
 # 리소스 그룹 생성
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
--- variables.tf
+++ variables.tf
@@ -1,16 +1,22 @@
 variable "resource_group_name" {
-  description = "GitHub 이슈에서 추출한 리소스 그룹 이름"
+  description = "배포할 리소스 그룹 이름"
   type        = string
 }
 
-variable "location" {
-  description = "Azure 리소스를 배포할 지역"
-  type        = string
-  default     = "Korea Central"
+variable "user_list" {
+  description = "접근 권한을 부여할 사용자 목록"
+  type        = list(string)
+  default     = []
 }
 
-variable "base_name" {
-  description = "기본 리소스 그룹 이름 접두사 (자동 넘버링이 필요할 경우)"
+variable "gpu_size" {
+  description = "필요한 GPU 크기"
   type        = string
-  default     = ""
+  default     = "해당 없음"
+}
+
+variable "environment" {
+  description = "배포 환경"
+  type        = string
+  default     = "개발(Development)"
 }
